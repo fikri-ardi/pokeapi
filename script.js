70491e4b817654cca99e5916140b84749badd026
fikri-dev <muhammadfikriardi@gmail.com>
2021-06-09 21:16:08 +0700

    fetch pokemons api with js

---
+++ script.js
@@ -0,0 +1,35 @@
+// onload
+// Fetch All Pokemons
+fetch('https://pokeapi.co/api/v2/pokemon')
+    .then( response => {
+        if (response.status != 200) {
+            console.log(`Oops ${response.status}`)
+            return
+        }
+        response.json().then( data => {
+            const pokemons = data.results
+            pokemons.forEach(pokemon => {
+                document.getElementById('pokemonList').insertAdjacentHTML('beforeend',
+                    `<li onclick="detail('${pokemon.url}')">${pokemon.name}</li>`
+                )
+            });
+        } )
+    }
+)
+
+// Show pokemon detail
+function detail(url) {
+    fetch(url).then(response => {
+        response.json().then(pokemon => {
+            document.getElementById('detail').innerHTML = '';
+            document.getElementById('detail').insertAdjacentHTML('beforeend',
+                `<img src="${pokemon.sprites.front_default}" class="pokemon">
+                <h3>${pokemon.name}</h3>
+                <p>${pokemon.height}cm</p>
+                <p>${pokemon.weight}kg</p>
+                <p>${pokemon.moves[0].move.name}</p>
+                `
+            )
+        })
+    })
+}
